e60ab0f0b8d9352501fc7d2c48832fb8f967f302
Alec Stephens <stephens.alec01@gmail.com>
2022-09-11 12:58:25 -0500

    added an animation

--- app.js
+++ app.js
@@ -40,7 +40,11 @@ function deleteCheck(e) {
     //Delete Todo 
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitioned', function() {
+            todo.remove();
+        });
     }
 
     //Check Mark
